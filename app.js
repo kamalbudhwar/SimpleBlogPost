7d19f55f20c7113e4e6763df13d82e38005f066b
Kamal <er.kamalsandhu@gmailcom>
2020-03-30 07:06:42 +1000

    Initial commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //jshint esversion:6
 
 const express = require("express");
+const mongoose=require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require("lodash");
-let posts=[];
+mongoose.connect('mongodb://localhost/blog',{useNewUrlParser: true, useUnifiedTopology: true });
+// let posts=[];
+var db=mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we're connected!");
+});
+var postSchema={
+  title:String,
+  content:String
+};
+var Post=mongoose.model('Post',postSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +31,26 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/',function(req,res){
-  res.render('home',{homeContent:homeStartingContent, postArray:posts});
+  Post.find({},function(err,foundPost){
+    if(!err){
+      if(!foundPost){
+        var post1=new Post({
+          title:"COVID-19 pandemic",
+          content:"Coronaviruses are a large family of viruses that can cause respiratory illness in humans and animals.COVID-19 is a new coronavirus – others include Severe Acute Respiratory Syndrome (SARS) and Middle East Respiratory Syndrome (MERS)."
+        });
+        var post2=new Post({
+          title:"COVID-19 Australia",
+          content:"Of the 3,966 confirmed cases in Australia, 16 have died from COVID-19. More than 211,000 tests have been conducted across Australia."
+        });
+        post1.save();
+        post2.save();
+        res.render('home',{homeContent:homeStartingContent, postArray:foundPost});
+      }
+      else{
+      res.render('home',{homeContent:homeStartingContent, postArray:foundPost});
+    }
+    }
+  });
 });
 
 
@@ -32,21 +64,28 @@ app.get("/compose",function(req,res){
   res.render('compose');
 });
 app.post("/compose",function(req,res){
-  const post={
+  const newpost=new Post({
     title:req.body.postTitle,
     content:req.body.postContent
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+  newpost.save(function(err){
+    if(!err){
+      res.redirect("/");
+    }
+  });
 });
 
-app.get('/posts/:postName',function(req,res){
-  const requestedTitle=_.lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    const storedTitle=_.lowerCase(post.title);
-    if(requestedTitle===storedTitle){
-      console.log("Match Found!!!!");
-      res.render('post',{postTitle:post.title, postContent:post.content});
+app.get('/posts/:postId',function(req,res){
+  const requestedId=req.params.postId;
+  Post.findOne({_id:requestedId},function(err,foundPost){
+     console.log(foundPost);
+    if(!err){
+      if(!foundPost){
+        console.log("Match Not Found!!!!");
+       }
+       else{
+         res.render('post',{postTitle:foundPost.title, postContent:foundPost.content});
+       }
     }
   });
 });
